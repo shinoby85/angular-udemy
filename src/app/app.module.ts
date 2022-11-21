b4effdb91b26b78c003b662f64af5150a6aca6a9
Evgeny Ponkratyev <eponkratyev@tula.co>
2022-11-22 00:13:56 +0300

    Add input and button for communicate with paragraph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { MessagesComponent } from './messages/messages.component';
-import {CategoryComponent} from "./category/category.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    MessagesComponent,
-    CategoryComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
