3fec4363d8aca6d46f6b185f8013babe3d01fd4c
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-25 10:32:51 +0300

    Getter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users"
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,4 +12,8 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
