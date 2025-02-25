bf1bcd7363447675a3b95a0c36ac0da59de2da17
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-25 15:18:48 +0300

    Use output function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input, output} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users"
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -14,7 +14,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return `assets/users/${this.avatar}`;
