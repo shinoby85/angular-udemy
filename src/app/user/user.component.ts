7596fd3e0201cbede46fe4ff31e31e7be763f0a9
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-25 17:34:06 +0300

    Use object like input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,16 +11,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: { id: string, name: string, avatar: string };
   select = output<string>();
 
   get imagePath() {
-    return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
