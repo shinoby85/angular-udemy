cd1fc1e4ca4e444adfee6ca41e6c77bcbe5d70ea
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-26 20:06:14 +0300

    Add wrapper around components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, output} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users"
+import {CardComponent} from "../shared/card/card.component";
 
 // type User = { id: string, name: string, avatar: string }
 
@@ -14,7 +15,9 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
