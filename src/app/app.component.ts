db0c2db76f6048eceafff5848acdb52d5807ab3c
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-27 19:19:00 +0300

    New demo project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,41 @@
-import {Component} from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {UserInputComponent} from "./user-input/user-input.component";
-import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [
-    HeaderComponent,
-    UserInputComponent,
-    InvestmentResultsComponent
-  ]
 })
 export class AppComponent {
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
