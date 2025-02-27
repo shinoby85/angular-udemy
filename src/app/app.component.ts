12209498c2b863f4d9e8b9bc108be3b95f78d602
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-27 11:48:26 +0300

    New project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,8 @@
-import {Component} from '@angular/core';
-import {DUMMY_USERS} from "./dummy-users";
-
-export interface User {
-  id: string,
-  name: string,
-  avatar: string,
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: false,
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-  selectedUser?: User
-
-  onSelectUser(id: string) {
-    this.selectedUser = this.users.find(user => user.id === id)!;
-  }
-}
-
-
+export class AppComponent {}
