3f5839d20d5fde020532ad6ac0e51c849b1828c0
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-28 10:46:02 +0300

    Update components

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,5 +8,5 @@ import {Component, Input} from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  @Input({required: true}) currentStatus?: string;
+  currentStatus = 'online';
 }
--- src/app/dashboard/traffic/traffic.component.ts
+++ src/app/dashboard/traffic/traffic.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-traffic',
@@ -8,6 +8,35 @@ import {Component, Input} from '@angular/core';
   styleUrl: './traffic.component.css'
 })
 export class TrafficComponent {
-  @Input({required: true}) dummyTrafficData?: { id: string; value: number }[];
-  @Input({required: true}) maxTraffic?: number
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
 }
