ab256d77011dfefd74f1466ce57be658356ab5d7
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-01 15:20:58 +0300

    HostBinding and HostListening

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -7,7 +7,19 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click', ['$event']) onClick(event: MouseEvent) {
+  //   console.log('click');
+  // }
   label = input<string>();
+
+  onClick() {
+    console.log('Clicked!!!');
+  }
 }
