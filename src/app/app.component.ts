2c2c90c475d0215def0a738b9eec0899c74a60fd
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-02 13:51:52 +0300

    lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
-import {Component} from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {ServerStatusComponent} from "./dashboard/server-status/server-status.component";
-import {TrafficComponent} from "./dashboard/traffic/traffic.component";
-import {TicketsComponent} from "./dashboard/tickets/tickets.component";
-import {DashboardItemComponent} from "./dashboard/dashboard-item/dashboard-item.component";
+import { Component } from '@angular/core';
+
+import { LifecycleComponent } from "./lifecycle/lifecycle.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  imports: [
-    HeaderComponent,
-    ServerStatusComponent,
-    TrafficComponent,
-    TicketsComponent,
-    DashboardItemComponent
-  ]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [LifecycleComponent]
 })
 export class AppComponent {
+  lifecycleComponentIsVisible = false;
+  lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
 
+  onToggleLifecycleComponentVisibility() {
+    this.lifecycleComponentIsVisible = !this.lifecycleComponentIsVisible;
+  }
+
+  onChangeLifecycleInputText() {
+    this.lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+  }
 }
