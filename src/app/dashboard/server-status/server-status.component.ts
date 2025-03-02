3d4557683841328568f562382dbbb75b64fcfb66
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-02 17:01:40 +0300

    Destroyed setInterval

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,10 +7,15 @@ import {Component} from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  interval?: ReturnType<typeof setInterval>;
 
   constructor() {
+
+  }
+
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();
 
@@ -22,5 +27,9 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 }
