d052b722b0e04d8e47f9828e780276ed8bc015c7
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-03 18:08:59 +0300

    New project for directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { RectComponent } from './rect/rect.component';
-import { FormsModule } from '@angular/forms';
+
+import { AuthComponent } from './auth/auth.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RectComponent, FormsModule],
+  styleUrl: './app.component.css',
+  imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {
-  rectSize = {
-    width: '100',
-    height: '100',
-  };
-}
+export class AppComponent {}
