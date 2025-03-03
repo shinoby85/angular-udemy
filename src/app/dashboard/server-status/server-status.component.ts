a498156a7e7f04443ed3a21e5b9ed1b19d8df6d3
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-03 12:09:39 +0300

    Use effect function

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import {Component, DestroyRef, inject, OnInit} from '@angular/core';
+import {Component, DestroyRef, effect, inject, OnInit, signal} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,21 +8,27 @@ import {Component, DestroyRef, inject, OnInit} from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
   // interval?: ReturnType<typeof setInterval>;
 
-  private _destroyRef = inject(DestroyRef)
+  private _destroyRef = inject(DestroyRef);
+
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus())
+    });
+  }
 
   ngOnInit() {
     const intervalId = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
     this._destroyRef.onDestroy(() => {
