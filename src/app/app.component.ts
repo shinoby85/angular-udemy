a748ee9cd650de810f703286e2f82e44ea3ea118
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-05 16:49:07 +0300

    New project (services)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,11 @@
-import {Component} from '@angular/core';
-import {DatePipe, DecimalPipe} from "@angular/common";
-import {TemperaturePipe} from "./temperature.pipe";
-import {SortPipe} from "./sort.pipe";
+import { Component } from '@angular/core';
+
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [
-    DatePipe,
-    DecimalPipe,
-    TemperaturePipe,
-    SortPipe
-  ]
+  imports: [TasksComponent],
 })
-export class AppComponent {
-  currentDate = new Date();
-  currentTemperaturs = {
-    berlin: 4.2749812,
-    newYork: 18.1214,
-    paris: 72.1209001,
-    chicago: 65.0775238,
-  };
-
-  historicTemperatures = [
-    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
-  ];
-
-  onReset(index: number) {
-    const newArray = [...this.historicTemperatures];
-    newArray[index] = 18;
-    this.historicTemperatures = newArray;
-
-  }
-}
+export class AppComponent {}
