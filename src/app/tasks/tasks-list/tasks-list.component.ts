c28241d3d6ef5f08dba279f3798846034585ca44
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-05 19:46:30 +0300

    Inject service to tasks-list and new-task components

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 
-import { TaskItemComponent } from './task-item/task-item.component';
+import {TaskItemComponent} from './task-item/task-item.component';
+import {TasksService} from "../tasks.service";
 
 @Component({
   selector: 'app-tasks-list',
@@ -11,7 +12,8 @@ import { TaskItemComponent } from './task-item/task-item.component';
 })
 export class TasksListComponent {
   selectedFilter = signal<string>('all');
-  tasks = [];
+  private tasksService = inject(TasksService);
+  tasks = this.tasksService.allTasks;
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
