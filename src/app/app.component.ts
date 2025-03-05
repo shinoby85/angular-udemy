b16c42a9317052b7b4f7bbb5c59394daf003cf48
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-05 15:52:56 +0300

    Sort array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,9 @@ export class AppComponent {
   ];
 
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    const newArray = [...this.historicTemperatures];
+    newArray[index] = 18;
+    this.historicTemperatures = newArray;
+
   }
 }
