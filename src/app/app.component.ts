c03986d7e5d6081afef7eed7928dc64f044556ae
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-07 17:30:27 +0300

    Convert signal to observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
+import {toObservable} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount)
 
+  ngOnInit() {
+    // interval(1000)
+    //   .pipe(
+    //     map(num => num * 2)
+    //   )
+    //   .subscribe({
+    //     next: (value) => {
+    //       console.log(value);
+    //     },
+    //     complete: () => {
+    //     },
+    //     error: () => {
+    //     }
+    //   });
+  }
 }
