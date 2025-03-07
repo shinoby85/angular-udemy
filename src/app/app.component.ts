7c8a7fa42e673a3554078155ac8126eca82ea63c
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-07 16:44:45 +0300

    Create new project (rxjs)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
-import { CounterComponent } from './counter/counter.component';
-import { MessagesComponent } from './messages/messages.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [CounterComponent, MessagesComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  get debugOutput() {
-    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
-    return 'AppComponent Component Debug Output';
-  }
+
 }
