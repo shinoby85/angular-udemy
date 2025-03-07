88978e581b71b2d36a6325383722fc4b158b0e52
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-07 12:53:06 +0300

    Use runOutsideAngular zone method

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, NgZone, OnInit, signal} from '@angular/core';
 
-import { InfoMessageComponent } from '../info-message/info-message.component';
+import {InfoMessageComponent} from '../info-message/info-message.component';
 
 @Component({
   selector: 'app-counter',
@@ -9,12 +9,25 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   count = signal(0);
+  private zone = inject(NgZone);
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0)
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Timer expired!');
+      }, 5000);
+    });
   }
 
   onDecrement() {
