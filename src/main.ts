d6c00bca34721c2a9736c38a7c79372fd66bc22b
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-08 17:55:46 +0300

    Add interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 
 import {AppComponent} from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
+import {HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {tap} from "rxjs";
+
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const req = request.clone({
+    // headers: request.headers.set('X-DEBUG', 'TESTING')
+  });
+  console.log('[Outgoing Request]');
+  console.log(request);
+  return next(req)
+    .pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log(event);
+          console.log('[Incoming Response]');
+        }
+      })
+    );
+}
 
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))]
 }).catch((err) => console.error(err));
