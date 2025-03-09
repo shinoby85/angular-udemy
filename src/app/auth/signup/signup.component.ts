382f6a7f565468983389addb1b4b140a708b4e00
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-09 15:20:52 +0300

    Create form model

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+    passwords: new FormGroup({
+      password: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
+      confirmPassword: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
+
+    }),
+    firstName: new FormControl('', {validators: [Validators.required]}),
+    lastName: new FormControl('', {validators: [Validators.required]}),
+    address: new FormGroup({
+
+      street: new FormControl('', {validators: [Validators.required]}),
+      city: new FormControl('', {validators: [Validators.required]}),
+      number: new FormControl('', {validators: [Validators.required]}),
+      postalCode: new FormControl('', {validators: [Validators.required]}),
+    }),
+    role: new FormControl('student', {validators: [Validators.required]}),
+    agree: new FormControl(false, {validators: [Validators.required]}),
+  })
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form);
+    this.form.reset();
+  }
+}
