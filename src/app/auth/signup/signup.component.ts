c6bc81fd876b2a50d42a650e4a10f0217f6d2cc2
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-09 15:57:24 +0300

    Check passwords

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,16 @@
 import {Component} from '@angular/core';
-import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+    if (val1 === val2) {
+      return null;
+    }
+    return {passwordsNotEqual: true}
+  }
+}
 
 @Component({
   selector: 'app-signup',
@@ -17,6 +28,8 @@ export class SignupComponent {
       password: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
       confirmPassword: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]}),
 
+    }, {
+      validators: [equalValues('password', 'confirmPassword')]
     }),
     firstName: new FormControl('', {validators: [Validators.required]}),
     lastName: new FormControl('', {validators: [Validators.required]}),
@@ -38,6 +51,7 @@ export class SignupComponent {
 
   onSubmit() {
     if (this.form.invalid) {
+      console.log('Invalid form.');
       return;
     }
     console.log(this.form);
