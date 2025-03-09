5522b365e32e0af1daebba4234052a3ed8e3a66d
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-09 14:22:55 +0300

    Custom control

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+  return {doesNotContainQuestionMark: true}
+}
 
 @Component({
   selector: 'app-login',
@@ -16,7 +23,7 @@ export class LoginComponent {
       validators: [Validators.required, Validators.email],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
+      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
     }),
   })
 
