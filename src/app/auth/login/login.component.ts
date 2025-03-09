893680f0f93077ea7c2c6553458cec211e510811
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-09 13:49:55 +0300

    Use onSubmit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,25 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
 export class LoginComponent {
+  myForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  onSubmit() {
+    console.log(this.myForm);
+    const enteredEmail = this.myForm.value.email;
+    const enteredPassword = this.myForm.value.password;
+    console.log(enteredEmail, enteredPassword);
+  }
 }
