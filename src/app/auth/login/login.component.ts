bec725ae1c5cdd8d0de4c782ae544b3567e92747
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-09 14:42:16 +0300

    Save data to localStorage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {debounceTime} from "rxjs";
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
     return null;
   }
   return {doesNotContainQuestionMark: true}
+}
+
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem("saved-login-form");
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
 }
 
 @Component({
@@ -17,15 +25,25 @@ function mustContainQuestionMark(control: AbstractControl) {
     ReactiveFormsModule
   ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  destroyRef = inject(DestroyRef);
   myForm = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.required, Validators.email],
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
     }),
   })
+
+  ngOnInit(): void {
+    const subscription = this.myForm.valueChanges.pipe(debounceTime(500)).subscribe(value => {
+      window.localStorage.setItem('saved-login-form', JSON.stringify(value));
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 
   controlIsInvalid(controlName: 'email' | 'password'): boolean {
     return this.myForm.controls[controlName].touched &&
