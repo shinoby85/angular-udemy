6cd2cdfd36487fff6cfeed10c1fbc146b614661c
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-10 19:47:27 +0300

    Create canDeactivate

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -2,7 +2,7 @@ import {Component, inject, input, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import {TasksService} from '../tasks.service';
-import {Router, RouterLink} from "@angular/router";
+import {CanDeactivateFn, Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-new-task',
@@ -30,4 +30,11 @@ export class NewTaskComponent {
     );
     this.router.navigate(['/users', this.userId(), 'tasks'], {replaceUrl: true});
   }
+}
+
+export const canLeaveEditPage: CanDeactivateFn<NewTaskComponent> = (component: NewTaskComponent) => {
+  if (component.enteredTitle() || component.enteredDate() || component.enteredSummary()) {
+    return window.confirm('Are you sure you want to leave?');
+  }
+  return true;
 }
--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
 
 import {resolveUserTasks, TasksComponent} from '../tasks/tasks.component';
-import {NewTaskComponent} from '../tasks/new-task/new-task.component';
+import {canLeaveEditPage, NewTaskComponent} from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +19,6 @@ export const routes: Routes = [
   {
     path: 'tasks/new',
     component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage]
   },
 ];
