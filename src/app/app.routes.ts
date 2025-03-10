8857836ee425e51c30e3f5cc01a40e249ff510ad
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-10 19:36:02 +0300

    Create canMatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,30 @@
-import {Routes} from "@angular/router";
+import {CanMatchFn, RedirectCommand, Router, Routes} from "@angular/router";
 import {NoTaskComponent} from "./tasks/no-task/no-task.component";
 import {resolveTitle, userNameResolver, UserTasksComponent} from "./users/user-tasks/user-tasks.component";
 import {routes as usersRoutes} from "./users/users.routes";
 import {NotFoundComponent} from "./not-found/not-found.component";
+import {inject} from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
-  {path: "", component: NoTaskComponent, title: 'No Tasks'},
+  {
+    path: "",
+    component: NoTaskComponent,
+    title: 'No Tasks'
+  },
   {
     path: 'users/:userId',
     component: UserTasksComponent,
     children: usersRoutes,
+    canMatch: [dummyCanMatch],
     resolve: {
       userName: userNameResolver
     },
