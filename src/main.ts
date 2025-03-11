b1bab0b9f1e0a631cb85ce7d5ffb49c33b9c8529
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 15:30:17 +0300

    Create reducer for standalone component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication} from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+import {AppComponent} from './app/app.component';
+import {counterReducer} from "./app/store/counter.reducer";
+import {provideStore} from "@ngrx/store";
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [provideStore({counter: counterReducer})],
+});
