7b89dc21038e423fd38359b2f6d45a7adb5797bb
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 15:26:51 +0300

    Create reducer for module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CounterOutputComponent } from './counter-output/counter-output.component';
-import { CounterControlsComponent } from './counter-controls/counter-controls.component';
-import { StoreModule } from '@ngrx/store';
+import {AppComponent} from './app.component';
+import {CounterOutputComponent} from './counter-output/counter-output.component';
+import {CounterControlsComponent} from './counter-controls/counter-controls.component';
+import {StoreModule} from '@ngrx/store';
+import {counterReducer} from "./store/counter.reducer";
 
 @NgModule({
   declarations: [
@@ -12,8 +13,11 @@ import { StoreModule } from '@ngrx/store';
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, StoreModule.forRoot({
+    counter: counterReducer,
+  })],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
