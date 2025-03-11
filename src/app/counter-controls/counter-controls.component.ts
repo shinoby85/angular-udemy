fb3b9eb4459f4dac0d4333a61d5c21ff00a8e535
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 18:22:06 +0300

    Add dispatch

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-
-import { CounterService } from '../counter.service';
+import {Component} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {decrement, increment} from "../store/counter.action";
 
 @Component({
   selector: 'app-counter-controls',
@@ -9,13 +9,14 @@ import { CounterService } from '../counter.service';
   standalone: true,
 })
 export class CounterControlsComponent {
-  constructor(private counterService: CounterService) {}
+  constructor(private store: Store<{ counter: number }>) {
+  }
 
   increment() {
-    this.counterService.increment();
+    this.store.dispatch(increment());
   }
 
   decrement() {
-    this.counterService.decrement();
+    this.store.dispatch(decrement());
   }
 }
