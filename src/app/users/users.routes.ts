6d5e14728aa762fca0a579f025d844b0fa7c73a3
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 12:42:21 +0300

    Create routes for prerender

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
-import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import {TasksComponent} from '../tasks/tasks.component';
+import {canLeaveEditPage, NewTaskComponent} from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
@@ -13,9 +13,6 @@ export const routes: Routes = [
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
     runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
   },
   {
     path: 'tasks/new',
