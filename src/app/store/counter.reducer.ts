3bdd73c4c9fcff626254db9d742a7bea88b2c306
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 18:53:15 +0300

    Use old style for counter reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,16 +1,21 @@
-import {createReducer, on} from "@ngrx/store";
-import {decrement, increment, myValue} from "./counter.action";
-
 const initialState = 0;
 
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, state => state + 1),
-  on(decrement, state => state - 1),
-  on(myValue, (state, action) => state + action.value),
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, state => state + 1),
+//   on(decrement, state => state - 1),
+//   on(myValue, (state, action) => state + action.value),
+// );
 
 //Old variant
-// export function counterReducer(state = initialState) {
-//   return state
-// }
+export function counterReducer(state = initialState, action: { type: string; value: number }) {
+  switch (action.type) {
+    case '[Counter] Increment':
+      return state + 1;
+    case '[Counter] Decrement':
+      return state - 1;
+    case '[Counter] MyValue':
+      return state + action.value;
+  }
+  return state
+}
