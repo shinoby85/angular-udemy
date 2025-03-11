951e72553775842b7ba079eca573c52c2ebba0ad
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 13:17:05 +0300

    Create new project (ngrx modules)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { HeaderComponent } from './header/header.component';
-import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [HeaderComponent, UsersComponent, RouterOutlet],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+}
