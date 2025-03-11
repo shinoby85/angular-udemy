863dfc4b813ce45946d8b0a55b8bc3a55249b260
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-11 10:47:33 +0300

    Create new project (deploying)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [WelcomeComponent],
+  imports: [HeaderComponent, UsersComponent, RouterOutlet],
 })
 export class AppComponent {}
