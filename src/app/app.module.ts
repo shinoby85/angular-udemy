2761a61f5edbec4fbf53509b59f1640520cda415
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-12 12:06:11 +0300

    Add effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {CounterOutputComponent} from './counter-output/counter-output.component'
 import {CounterControlsComponent} from './counter-controls/counter-controls.component';
 import {StoreModule} from '@ngrx/store';
 import {counterReducer} from "./store/counter.reducer";
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import {counterReducer} from "./store/counter.reducer";
   ],
   imports: [BrowserModule, StoreModule.forRoot({
     counter: counterReducer,
-  })],
+  }), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
