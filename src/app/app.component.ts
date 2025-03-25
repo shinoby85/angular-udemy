7736fdd9c9eec19c710d85f5d8c651c8b0cbe618
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-03-25 09:50:44 +0300

    New project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
-import { LoginComponent } from './auth/login/login.component';
-
 @Component({
   selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  imports: [LoginComponent],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item) {
+      this.list.push(item);
+    }
+}
