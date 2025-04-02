24b29d5a4911da64955a1cc94f49af29447b6b69
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-04-02 23:43:48 +0300

    Add user model and redirect

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import {Component, inject} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from './auth.service';
+import {AuthService, IAuthResponseData} from './auth.service';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,6 +14,7 @@ export class AuthComponent {
   public isLoading = false;
   public error?: string;
   private _authService = inject(AuthService);
+  private _router = inject(Router);
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -21,26 +24,29 @@ export class AuthComponent {
     if (form.invalid) {
       return;
     }
+    let authObs$: Observable<IAuthResponseData>;
     this.isLoading = true;
     const email = form.value.email;
     const password = form.value.password;
 
     if (this.isLoginMode) {
-
+      authObs$ = this._authService.login(email, password);
     } else {
-      this._authService.signUp(email, password).subscribe({
-        next: (respData) => {
-          console.log(respData);
-          this.isLoading = false;
-        },
-        error: (errorMessage) => {
-          this.isLoading = false;
-          this.error = errorMessage;
-          console.log(errorMessage);
-        }
-      });
+      authObs$ = this._authService.signUp(email, password);
     }
 
+    authObs$.subscribe({
+      next: (respData) => {
+        console.log(respData);
+        this.isLoading = false;
+        this._router.navigate(['/recipes']);
+      },
+      error: (errorMessage) => {
+        this.isLoading = false;
+        this.error = errorMessage;
+        console.log(errorMessage);
+      }
+    });
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {catchError, throwError} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError, Subject, throwError} from 'rxjs';
+import {User} from './user.model';
+import {tap} from 'rxjs/operators';
 
 const FIREBASE_API_KEY = 'AIzaSyBYc6aiUBH00e6qrVw4TxwjFT2Ajk0d0a4';
 
@@ -10,12 +12,14 @@ export interface IAuthResponseData {
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user = new Subject<User>();
 
   http = inject(HttpClient);
 
@@ -26,24 +30,67 @@ export class AuthService {
       returnSecureToken: true
     })
       .pipe(
-        catchError(err => {
-          let errorMessage = 'An unknown error occurred!';
-          if (!err.error || !err.error.error) {
-            return throwError(() => new Error(errorMessage));
-          }
-          switch (err.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'The email address is already in use by another account.';
-              break;
-            case 'OPERATION_NOT_ALLOWED':
-              errorMessage = 'Password sign-in is disabled for this project';
-              break;
-            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-              errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-              break;
-          }
-          return throwError(() => new Error(errorMessage));
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
         })
       );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<IAuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`, {
+      email,
+      password,
+      returnSecureToken: true
+    }).pipe(
+      catchError(this.handleError),
+      tap((resData) => {
+        this.handleAuthentication(
+          resData.email,
+          resData.localId,
+          resData.idToken,
+          +resData.expiresIn
+        );
+      })
+    );
+  }
+
+  private handleAuthentication(email: string, localId: string, idToken: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, localId, idToken, expirationDate);
+    this.user.next(user);
+  };
+
+  private handleError(errorResp: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorResp.error || !errorResp.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+    switch (errorResp.error.error.message) {
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator.';
+        break;
+      case 'EMAIL_EXISTS':
+        errorMessage = 'The email address is already in use by another account.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
